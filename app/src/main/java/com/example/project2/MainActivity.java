47377b16d59fec530e89d2658f9c519fc30d558f
Amen <krakenamen@gmail.com>
2025-11-03 23:55:01 +0100

    ve

--- app/src/main/java/com/example/project2/MainActivity.java
+++ app/src/main/java/com/example/project2/MainActivity.java
@@ -1,20 +1,41 @@
 package com.example.project2;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
+import androidx.core.view.ViewCompat;sh
 import androidx.core.view.WindowInsetsCompat;
+
+import java.text.BreakIterator;
+
 
 public class MainActivity extends AppCompatActivity {
 
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.linear);
+        final EditText Login;
+        final Button b1;
+        Login =(EditText) findViewById(R.id.editTextText);
+        b1 =(Button) findViewById(R.id.button3);
+        b1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Toast msg = Toast.makeText(getBaseContext(), Login.getText().toString(),Toast.LENGTH_LONG);
+                msg.show();
+            }
+        });
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
